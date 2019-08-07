daeff4f3da27518bf3fffdfb3c5126c94af12b95
Mirakklian <leopratik80@gmail.com>
2019-08-08 01:39:47 +0530

    smallest substring that contain all the distinct character-V.01

---
+++ Maximal_pratik_vit.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maximl_pratik_vit;
+import java.util.*;
+
+/**
+ *
+ * @author Pratik Dutta
+ */
+
+public class Maximal_pratik_vit {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int getLength(String str) {
+    int preInd = 0, currInd = 0, maxLen = 0;
+    int strLen = str.length();
+    HashSet<Character> characterSet = new HashSet<>();
+
+    while (currInd < strLen) {
+      if (characterSet.contains(str.charAt(currInd))) {
+        characterSet.remove(str.charAt(preInd));
+        preInd++;
+      } 
+      else {
+        characterSet.add(str.charAt(currInd));
+        currInd++;
+        maxLen = Math.max(maxLen, currInd - preInd);
+      }
+    }
+    return maxLen;
+  }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        //HashSet<Character> h = new HashSet(); 
+        
+        String s;
+        System.out.println("Enter the String: ");
+        s=sc.next();
+        System.out.println(getLength(s));
+   }
+}
